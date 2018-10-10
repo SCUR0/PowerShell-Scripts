f67e4487849d980c769c0d1410d489d0a5e0305e
SCUR0 <joelm04@gmail.com>
2018-10-10 11:15:54 -0700

    Update Disable-UpdateReboot.ps1
    Added additional error handling.

--- Disable-UpdateReboot.ps1
+++ Disable-UpdateReboot.ps1
@@ -27,6 +27,7 @@ If (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]
 }
 
 #Variables
+$Errors=$null
 $RebootReg="Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UpdateOrchestrator\Reboot"
 $RebootTask="$env:WinDir\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot"
 
@@ -44,38 +45,51 @@ $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey(`
     "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UpdateOrchestrator\Reboot",`
     [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree,[System.Security.AccessControl.RegistryRights]::ChangePermissions)
 $acl = $key.GetAccessControl()
-$rule = New-Object System.Security.AccessControl.RegistryAccessRule ("BUILTIN\Administrators","FullControl","Allow")
-$acl.ResetAccessRule($rule)
-$rule = New-Object System.Security.AccessControl.RegistryAccessRule ("System","ReadKey","Allow")
-$acl.SetAccessRule($rule)
-$key.SetAccessControl($acl)
+if ($null -eq $acl){
+    Write-Warning "Error encountered while trying to modify registry key for reboot"
+    $Errors=$true
+}else{
+    $rule = New-Object System.Security.AccessControl.RegistryAccessRule ("BUILTIN\Administrators","FullControl","Allow")
+    $acl.ResetAccessRule($rule)
+    $rule = New-Object System.Security.AccessControl.RegistryAccessRule ("System","ReadKey","Allow")
+    $acl.SetAccessRule($rule)
+    $key.SetAccessControl($acl)
 
-#remove inheritance
-$acl = Get-Acl -Path $RebootReg
-$acl.SetAccessRuleProtection($true,$false)
-$acl | Set-Acl
+    #remove inheritance
+    $acl = Get-Acl -Path $RebootReg
+    $acl.SetAccessRuleProtection($true,$false)
+    $acl | Set-Acl
+}
 
 #SET ACL for task file
 #Change owner
 Write-Verbose "Modifying scheduled task files." -Verbose
-$acl = Get-ACL -Path $RebootTask
-$Group = New-Object System.Security.Principal.NTAccount("BUILTIN\Administrators")
-$acl.SetOwner($Group)
-Set-Acl -Path $RebootTask -AclObject $acl
+$acl = Get-ACL -Path $RebootTask -ErrorAction SilentlyContinue
+if ($null -eq $acl){
+    Write-Warning "Error encountered while trying to modify registry key for reboot"
+    $Errors=$true
+}else{
+    $Group = New-Object System.Security.Principal.NTAccount("BUILTIN\Administrators")
+    $acl.SetOwner($Group)
+    Set-Acl -Path $RebootTask -AclObject $acl
 
-#remove inheritance 
-$acl = Get-Acl -Path $RebootTask
-$acl.SetAccessRuleProtection($true,$false)
-$acl | Set-Acl -ErrorAction Stop
+    #remove inheritance 
+    $acl = Get-Acl -Path $RebootTask
+    $acl.SetAccessRuleProtection($true,$false)
+    $acl | Set-Acl -ErrorAction Stop
 
-#remove and set permissions
-$acl = Get-ACL -Path $RebootTask
-$acl.Access | %{$acl.RemoveAccessRule($_)} |Out-Null
-$ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$env:Username","FullControl","Allow")
-$acl.SetAccessRule($ar)
-$ar = New-Object  system.security.accesscontrol.filesystemaccessrule("System","ReadAndExecute","Allow")
-$acl.SetAccessRule($ar)
-Set-Acl -Path $RebootTask -AclObject $acl
-
-Write-Verbose "Script complete." -Verbose
-Write-Warning "Script will need to be run again after a feature (new windows build) update."
+    #remove and set permissions
+    $acl = Get-ACL -Path $RebootTask
+    $acl.Access | %{$acl.RemoveAccessRule($_)} |Out-Null
+    $ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$env:Username","FullControl","Allow")
+    $acl.SetAccessRule($ar)
+    $ar = New-Object  system.security.accesscontrol.filesystemaccessrule("System","ReadAndExecute","Allow")
+    $acl.SetAccessRule($ar)
+    Set-Acl -Path $RebootTask -AclObject $acl
+}
+if (!$Errors){
+    Write-Verbose "Script complete." -Verbose
+    Write-Warning "Script will need to be run again after a feature (new windows build) update."
+}else{
+    Write-Output "Errors were encountered while attempting to make changes. The script was not successful."
+}
