6991385c31cf51b66bdd86feaa2294722df635f3
SCUR0 <joelm04@gmail.com>
2018-10-24 10:36:11 -0700

    Update Export-VMs.ps1
    Changed variable to be parameter and added total progress bar.

--- Tasks/Export-VMs.ps1
+++ Tasks/Export-VMs.ps1
@@ -5,17 +5,24 @@
 .DESCRIPTION
   The script will copy all VMs to a directory of your choice.
   This is usefull for backup.
+
+.PARAMETER ExportPath
+  If left empty will use D:\Backup\VMs, set to your folder for vm backup or edit default in script.
 #>
 [cmdletbinding()]
-param ()
+param (
+    $ExportPath="D:\Backup\VMs"
+)
 
-#change path to location you want the copies stored.
-$ExportPath="D:\Backup\VMs"
 $VMs=Get-VM
+$VMCount=$VMs.count
+$CurrentCount=0
 foreach ($VM in $VMs) {
+    $Percent=[math]::Round($CurrentCount/$VMCount*100)
+    Write-Progress -Activity "Backing up VMs" -Status "Exporting $($VM.Name) $($CurrentCount+1)/$VMCount" -PercentComplete $Percent -Id 1
     $RemoveFail=$null
     if (Test-Path -Path $ExportPath\$($VM.Name)) {
-        Write-Verbose "Previous backup found, moving temp."
+        Write-Verbose "Previous backup found, moving temp." -Verbose
         try{
             Rename-Item -Path $ExportPath\$($VM.Name) -NewName "$ExportPath\$($VM.Name).temp" -ErrorAction Stop
         }catch{
@@ -23,18 +30,19 @@ foreach ($VM in $VMs) {
         }
     } 
     if (!$MoveFail){
-        write-verbose "Exporting VM: $($VM.Name)."
+        write-verbose "Exporting VM: $($VM.Name)." -Verbose
         try{
             Export-VM -Name $VM.Name -Path $ExportPath -ErrorAction Stop
         }catch{
-            $ExportFail = $true
             Write-Warning "Export failed, reverting backup."
             Rename-Item -Path "$ExportPath\$($VM.Name).temp" -NewName "$ExportPath\$($VM.Name)"
-
+            $ExportFail = $true
         }
         if (!$ExportFail){
-            Write-Verbose "Export successful. Deleting old backup."
+            Write-Verbose "Export successful. Deleting old backup." -Verbose
             Remove-Item -Path "$ExportPath\$($VM.Name).temp" -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue
         }
     }
+    $CurrentCount++
 }
+Write-Progress -Activity "Backing up VMs" -Completed
