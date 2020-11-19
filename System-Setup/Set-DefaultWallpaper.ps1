61ae0c1423e5991ad56c326f7a70f54e8c88c39b
SCUR0 <joelm04@gmail.com>
2020-11-19 12:34:55 -0800

    Removed unneeded variable and changed grant to group instead of account.

--- System-Setup/Set-DefaultWallpaper.ps1
+++ System-Setup/Set-DefaultWallpaper.ps1
@@ -1,5 +1,5 @@
 function RefreshWallpaper ($wallpaper){
-    Add-Type @‚Äù
+    Add-Type @î
         using System;
         using System.Runtime.InteropServices;
         using Microsoft.Win32;
@@ -13,27 +13,24 @@ function RefreshWallpaper ($wallpaper){
                 }
             }
         }
-‚Äú@
+ì@
     Write-Debug 'The wallpaper being refreshed is: $wallpaper'
     [Wallpaper.UpdateImage]::Refresh($wallpaper)
     Write-Debug 'Post Wallpaper refresh TranscodeImagecache value: $(ConvertFrom-Hexa)'
 }
 
-#Variables
-$ScriptRoot = Split-Path -parent $PSCommandPath
-
 #Set permissions
 takeown /f $env:windir\Web\Wallpaper\Windows\img0.jpg
 takeown /f $env:windir\Web\4K\Wallpaper\Windows\*.*
-icacls $env:windir\Web\Wallpaper\Windows\img0.jpg /Grant 'Administrator:(F)'
-icacls $env:windir\Web\4K\Wallpaper\Windows\*.* /Grant 'Administrator:(F)'
+icacls $env:windir\Web\Wallpaper\Windows\img0.jpg /Grant 'Administrators:(F)'
+icacls $env:windir\Web\4K\Wallpaper\Windows\*.* /Grant 'Administrators:(F)'
  
 #Rename default wallpaper
 Rename-Item $env:windir\Web\Wallpaper\Windows\img0.jpg img1.jpg -Force -ErrorAction SilentlyContinue
  
 #Copy new default wallpaper
-Copy-Item $ScriptRoot\images\img0.jpg $env:windir\Web\Wallpaper\Windows -Force
+Copy-Item $PSScriptRoot\images\img0.jpg $env:windir\Web\Wallpaper\Windows -Force
 #Copy alternative resolutions
-Copy-Item $ScriptRoot\images\4K\*.* $env:windir\Web\4K\Wallpaper\Windows -Force
+Copy-Item $PSScriptRoot\images\4K\*.* $env:windir\Web\4K\Wallpaper\Windows -Force
 
 RefreshWallpaper "$env:windir\Web\Wallpaper\Windows\img0.jpg"
