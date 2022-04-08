2348bef9650d269f0c9b2c99f155a1fb69f48b89
SCUR0 <joelm04@gmail.com>
2022-04-08 07:55:56 -0700

    Update Set-NTFSCompression.ps1   Added state parameter

--- Tasks/Set-NTFSCompression.ps1
+++ Tasks/Set-NTFSCompression.ps1
@@ -2,26 +2,38 @@
 .SYNOPSIS
   Sets NTFS compression to directories.
 
+.PARAMETER  Path
+	Path to location that compression needs to be checked. Supports multiple paths.
+
+.PARAMETER  State
+	State of file compression you want set.
+
 .DESCRIPTION
-  Scans folders saved in variable recursively to check for files not compressed.
-  Any files found not compressed, NTFS basic compression will apply.
+  Scans folders recursively to check for ntfs compression.
+  Any files found not in the requested compression state will be changed.
 #>
-[cmdletbinding()]
-param ()
 
-#Folder directories to be checked for compression
-#The ones below are examples.
-
-$compressfolders=(
-  "D:\Backup",
-  "D:\Shared\Video Projects",
-  "D:\Shared\Software"
+[CmdletBinding()]
+param (
+	$Path,
+	[ValidateSet('compress','uncompress')]
+	[string]$State = 'compress'
 )
+if ($State -eq 'uncompress'){
+	$CMDState = '/U'
+}else{
+	$CMDState = '/C'
+}
 
+foreach ($folder in $Path) {
+    $Parent = Get-ChildItem $folder -Recurse
+    if ($CMDState -eq 'compress'){
+        $Children = $Parent | where-object {$_.Attributes -notlike "*Compressed*"}
+    }else{
+        $Children = $Parent | where-object {$_.Attributes -like "*Compressed*"}
+    }
 
-foreach ($folder in $compressfolders) {
-    Get-ChildItem $folder -Recurse| where-object {$_.Attributes -notlike "*Compressed*"} | 
-    ForEach-Object {
-        compact /C $_.FullName
+    foreach ($Child in $Children){
+        compact $CMDState $Child.FullName
     }
 }
