27cb8bdb29bab35a20f953811632537cd9c0628c
SCUR0 <joelm04@gmail.com>
2022-04-29 14:47:14 -0700

    Update Set-NTFSCompression.ps1   Optimization pass and added file exclusions

--- Tasks/Set-NTFSCompression.ps1
+++ Tasks/Set-NTFSCompression.ps1
@@ -3,10 +3,16 @@
   Sets NTFS compression to directories.
 
 .PARAMETER  Path
-	Path to location that compression needs to be checked. Supports multiple paths.
+	Path to location that compression needs to be checked. Supports arrays.
+
+.PARAMETER  Exclude
+	File extensions to exclude from compression. Common compressed files set as default.
 
 .PARAMETER  State
 	State of file compression you want set.
+
+.PARAMETER  Progress
+	Show progress bar. Recommended to not be shown if running as a task due to overhead. Use -Progress:$false to hide.
 
 .DESCRIPTION
   Scans folders recursively to check for ntfs compression.
@@ -15,25 +21,50 @@
 
 [CmdletBinding()]
 param (
+    [Parameter(Mandatory)]
 	$Path,
+    $Exclude = ('*.7z', '*.zip','*.mp4','*.mp3','*.mov','*.mkv'), 
 	[ValidateSet('compress','uncompress')]
-	[string]$State = 'compress'
+	[string]$State = 'compress',
+    [switch]$Progress = $true
 )
-if ($State -eq 'uncompress'){
-	$CMDState = '/U'
+
+if ($State -eq 'compress'){
+    $StateString = 'Compressing'
 }else{
-	$CMDState = '/C'
+    $StateString = 'Uncompressing'
 }
 
-foreach ($folder in $Path) {
-    $Parent = Get-ChildItem $folder -Recurse
-    if ($CMDState -eq 'compress'){
-        $Children = $Parent | where-object {$_.Attributes -notlike "*Compressed*"}
-    }else{
-        $Children = $Parent | where-object {$_.Attributes -like "*Compressed*"}
-    }
+foreach ($Folder in $Path) {
+	Write-Verbose "Analysing $Folder" -Verbose
+    $Parent = Get-ChildItem $Folder -Recurse -Exclude $Exclude
 
-    foreach ($Child in $Children){
-        compact $CMDState $Child.FullName
+    if ($Progress){
+        $Total = $Parent.Count
+        $i = 1
+        #Adjust update interval based on total amount
+        if ($Total -le 1000){
+            $Division = 1
+        }elseif ($Total -ge 10000){
+            $Division = 100
+        }else{
+            $Division = 10
+        }
+    }
+    
+
+    foreach ($Child in $Parent){
+        if ($Progress -and ($i % $Division -eq 0)){
+            Write-Progress -Activity "$StateString Data" -Status "$i/$Total $($Child.Directory.FullName)"  -PercentComplete ($i / $Total * 100)
+        }
+        $i++
+        if ($State -eq 'compress' -and $Child.Attributes -notlike '*Compressed*'){
+            compact /C $Child.FullName /q > $null
+        }elseif($State -eq 'uncompress' -and $Child.Attributes -like '*Compressed*'){
+            compact /U $Child.FullName /q > $null
+        }
+    }
+    if ($Progress){
+        Write-Progress -Activity "$StateString Data" -Completed
     }
 }
